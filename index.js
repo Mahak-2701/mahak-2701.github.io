ef989c7e17adf1b5c4c405edd783b46206628738
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-08-26 20:50:28 +0530

    $

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const root = document.documentElement;
 const head = document.head;
 const body = document.body;
 
-function page(){
+async function page(){
     console.log(document.all);
     return `Collection of All Elements : ${document.all}\n`;
 }
 async function greet(){
     let p = await page();
-    console.log("\t Hello All Users: ", document.querySelectorAll("*"));
+    setTimeout(() => {
+        console.log("\t Hello All Users: ", document.querySelectorAll("*"));
+    }, 2000);
 }
 document.addEventListener("DOMContentLoaded", async() =>{
     let first = await greet();
-
+    let body = document.body;
     const div = document.createElement("code");
     div.innerHTML = `<h2>I was created on runtime. <h2/>`;
     body.appendChild(div);
