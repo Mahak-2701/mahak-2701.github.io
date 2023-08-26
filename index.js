94924607adef157fa52a88a8f8826875dad076bd
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-08-26 20:26:35 +0530

    $

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const root = document.documentElement;
 const head = document.head;
 const body = document.body;
+
+async function page(){
+    console.log(document.all);
+    return `Collection of All Elements: ${document.all}\n`;
+}
